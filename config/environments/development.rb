fd3f09ab1cf081b7e62c0d5ed659cfdc72294e52
Kiran Banmala <kiran@gurzu.net>
2021-09-08 07:42:04 +0545

    deployed in heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    # domain:               'gmail.com',
+    # domain:             'gmail.com',
     user_name:            'kirantestacc@gmail.com',
     password:             'password 123',
     authentication:       'plain'
